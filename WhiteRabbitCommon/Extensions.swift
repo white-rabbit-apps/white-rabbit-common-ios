a06c8f705a60bdee46d1dd97bac0223f8b6fbe79
Michael Bina <michaelbina@icloud.com>
2016-04-19 22:05:12 -0700

    update extensions

--- WhiteRabbitCommon/Extensions.swift
+++ WhiteRabbitCommon/Extensions.swift
@@ -13,11 +13,11 @@ extension UIColor {
         return UIColor(red: 0/255, green: 42/255, blue: 79/255, alpha: 1.0)
     }
     
-    public func secondaryColor() -> UIColor {
+    public class func secondaryColor() -> UIColor {
         return UIColor.lightGrayColor()
     }
     
-    public func linkColor() -> UIColor {
+    public class func linkColor() -> UIColor {
         return UIColor(red: 0/255, green: 94/255, blue: 183/255, alpha: 1.0)
     }
 }
